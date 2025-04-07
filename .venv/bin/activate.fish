f7fce451de76e5a462e9fe08047948b40f9165c1
Ashton <ashtonrwsmith.co@gmail.com>
2025-04-07 09:48:32 -0500

    This now includes drone J and some needed testing files/scripts

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/goose/Downloads/AppGithub/.venv
+set -gx VIRTUAL_ENV /home/goose/apps/crow/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
